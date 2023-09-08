df3f18faae3badae80e7c6827ca8f483d6aec863
thejjwang <jonathanjacobwang@gmail.com>
2023-09-08 16:37:46 -0700

    added bg image

--- vite-project/src/components/HomePage.jsx
+++ vite-project/src/components/HomePage.jsx
@@ -3,7 +3,7 @@
 const HomePage = () => {
 
     return (
-        <div id="home" className="h-screen w-screen bg-sky-500 flex flex-col justify-center items-center text-white">
+        <div id="home" className="h-screen w-screen bg-[url(https://wearecardinals.com/wp-content/uploads/2020/04/u1Re9qgMfM8d6kumlW85PS6s55jQh5fbdmppgQsP.jpeg)] bg-no-repeat bg-cover flex flex-col justify-center items-center text-white">
             <div className="text-center">
                 <h1 className="text-4xl font-bold mb-4">Welcome to my Sports Event Tracker!</h1>
                 <h3 className="text-lg">This application is made so you can easily track/manage your sports events</h3>
