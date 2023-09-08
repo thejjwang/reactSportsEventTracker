a598e775315e3b45db6120349a678816bb277266
thejjwang <jonathanjacobwang@gmail.com>
2023-09-08 13:03:31 -0700

    restyled buttons

--- vite-project/src/components/EventsPage.jsx
+++ vite-project/src/components/EventsPage.jsx
@@ -6,7 +6,7 @@ import Event from "./Event";
 const EventsPage = ({events}) => {
 
     return (
-        <div id="events" className="h-screen w-screens bg-sky-300 pt-28">
+        <div id="events" className="h-screen w-screens bg-sky-300 pt-24">
             {events.map((event, index) => (
                 <div className="bg-white m-4 p-4 rounded-lg shadow-md w-64 h-48 flex flex-col justify-center items-center mt-0" key={index}>
                     <Event event={event}/>
